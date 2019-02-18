b89312536692401e8d93e6b58b51f9230419d77d
Rajasegar <rajasegarc@freshdesk.com>
2019-02-18 13:07:52 +0530

    Removing unnecessary code from source.js

--- source.js
+++ source.js
@@ -20,22 +20,11 @@ export default Service.extend({
   accessTokenRenewalUrl: null,
   callbacksAwaitingAccessToken: [],
   httpProxyUrl: null,
-  installedApps: computed(function() {
-    return get(this, 'store').findAll('installed-application');
-  }),
-
   payloadList: [],
   awaitingAccessToken: false,
   init() {
     this._super(...arguments);
-  },
-  initialize(options) {
-    set(this, 'options', options || {});
-    get(this, 'options.auth_type') == 'OAuth' ? set(this, 'options.refresh_count', 0) : null;
-    set(this, 'appName', options.app_name || 'Integrated Application');
-    set(this, 'accessTokenRenewalUrl', options.is_customer ? HTTP_REQUEST_URL.accessTokenForCustomer : HTTP_REQUEST_URL.accessToken);
-    set(this, 'httpProxyUrl', options.is_customer ? HTTP_REQUEST_URL.requestProxyForCustomer : HTTP_REQUEST_URL.requestProxy);
-  },
+  }
 
   
 });
