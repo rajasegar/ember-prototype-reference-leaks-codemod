e2fc0a29d9efde328b60587ed59bcdc513b1a971
Rajasegar <rajasegarc@freshdesk.com>
2019-02-18 12:55:04 +0530

    Updating transform.js with edge cases to address
    1. Ignoring component props like classNames, etc.,

--- transform.js
+++ transform.js
@@ -1,44 +1,58 @@
-// Press ctrl+space for code completion
+// TODO
+// 1. Deal with secondary extend arguments like dec.arguments[1] if it is there
+// 2. Need to impletement for Object values apart from arrays
 export default function transformer(file, api) {
   const j = api.jscodeshift;
   const root = j(file.source);
+  const ignoreProps = [
+    'classNames', 
+    'classNameBindings',
+    'attributeBindings',
+    'queryParams'
+  ];
 
   root.find(j.ExportDefaultDeclaration).forEach(path => {
     let dec = path.value.declaration;
-    let props = dec.arguments[0].properties;
+    if(dec.arguments && dec.arguments[0]) {
+      let props = dec.arguments[0].properties;
 
-    let initFn = props.filter(p => p.value.type === "FunctionExpression" && p.key.name === "init")[0];
 
-    let initFnBody;
-    if(initFn) {
-          initFnBody = initFn.value.body.body;
-    } else {
+      if(props) { // If empty properties
+        let initFn = props.filter(p => p.value.type === "FunctionExpression" && p.key.name === "init")[0];
 
-      let superCall = j.expressionStatement(j.callExpression(j.memberExpression(j.thisExpression(),j.identifier('_super'),false),[j.identifier('...arguments')]))
-      let initProp = j.property("init",j.identifier('init'), j.functionExpression(null,[],j.blockStatement([superCall]),false,false));
+        let initFnBody;
 
-      props.push(initProp);
-      initFn = props.filter(p => p.value.type === "FunctionExpression" && p.key.name === "init")[0];
-      initFnBody = initFn.value.body.body;
+
+        //console.log(props);
+
+        let arrExps = props.filter(p => p.value.type === "ArrayExpression");
+
+        // Adding new set expressions for each array expression
+        
+        arrExps.filter(p => !ignoreProps.includes(p.key.name)) // Ignoring classNames for components
+          .forEach(p => {
+          //console.log(p.key.name);
+          let setExp = j.expressionStatement(j.callExpression(j.identifier("set"), [j.identifier("this"), j.identifier(`'${p.key.name}'`), j.arrayExpression(p.value.elements)]));
+          if(initFn) {
+            initFnBody = initFn.value.body.body;
+          } else {
+
+            let superCall = j.expressionStatement(j.callExpression(j.memberExpression(j.thisExpression(),j.identifier('_super'),false),[j.identifier('...arguments')]))
+            let initProp = j.property("init",j.identifier('init'), j.functionExpression(null,[],j.blockStatement([superCall]),false,false));
+
+            props.push(initProp);
+            initFn = props.filter(p => p.value.type === "FunctionExpression" && p.key.name === "init")[0];
+            initFnBody = initFn.value.body.body;
+          }
+          initFnBody.push(setExp);
+          // Removing array expression values from properties
+          props.splice(props.findIndex(p => p.value.type === "ArrayExpression" && !ignoreProps.includes(p.key.name)),1);
+
+        });
+
+      }
     }
 
-    console.log(props);
-
-    let arrExps = props.filter(p => p.value.type === "ArrayExpression");
-
-    // Adding new set expressions for each array expression
-    arrExps.forEach(p => {
-      console.log(p.key.name);
-
-       let setExp = j.expressionStatement(j.callExpression(j.identifier("set"), [j.identifier("this"), j.identifier(`'${p.key.name}'`), j.arrayExpression(p.value.elements)]));
-      initFnBody.push(setExp);
-          // Removing array expression values from properties
-    props.splice(props.findIndex(p => p.value.type === "ArrayExpression"),1);
-  
-  
-    });
-
-   
   });
 
   return root.toSource();
